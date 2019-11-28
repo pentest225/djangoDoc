407fca97deef436d15660ab2d2818fa647a22efa
pentest225 <47216215+pentest225@users.noreply.github.com>
2019-11-28 15:15:15 +0000

    ok Creations des models et premierre redactions du readme.md

--- my_project/myApp/admin.py
+++ my_project/myApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Categorie)
+class CategorieAdmin(admin.ModelAdmin):
+    list_display =('nom','description','image','date_add','date_upd')
--- my_project/myApp/models.py
+++ my_project/myApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Categorie(models.Model):
+    nom = models.CharField(max_length=250)
+    description = models.TextField()
+    image = models.ImageField(upload_to="Categorie/%Y/%m/%d/")
+    date_add = models.DateTimeField( auto_now_add=True)
+    date_upd = models.DateTimeField( auto_now=True)
+    status = models.BooleanField(default=False)
+    
+class Produit(models.Model):
+    nom=models.CharField(max_length=255)
+    description=models.TextField()
+    image=models.ImageField(upload_to='Produit/')
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE,related_name='categorie_produit')
+    date_add=models.DateTimeField(auto_now=True)
+    date_upd=models.DateTimeField(auto_now_add=True)
+    status=models.BooleanField(default=False)
